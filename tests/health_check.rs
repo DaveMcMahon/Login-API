2283aa3faabc36cd8a994cc8d06eecff51437fcf
DaveMcMahon <davidmcmhn@gmail.com>
2023-05-28 21:40:42 +0100

    test

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,5 @@
-use std::net::TcpListener;
 use login_api::run;
+use std::net::TcpListener;
 
 #[tokio::test]
 async fn health_check_works() {
@@ -7,18 +7,19 @@ async fn health_check_works() {
 
     let client = reqwest::Client::new();
 
-    let response = client.get(&format!("{}/health_check", &address))
-                        .send()
-                        .await
-                        .expect("Couldn't make the request");
+    let response = client
+        .get(&format!("{}/health_check", &address))
+        .send()
+        .await
+        .expect("Couldn't make the request");
 
     assert_eq!(Some(0), response.content_length());
 }
 
 fn spawn_app() -> String {
-   let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port"); 
-   let port  = listener.local_addr().unwrap().port();
-   let server = run(listener).expect("Failed to bind address");
-   let _ = tokio::spawn(server);
-   format!("http://127.0.0.1:{}", port)
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = run(listener).expect("Failed to bind address");
+    let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
 }
