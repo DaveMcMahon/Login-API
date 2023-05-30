cfa09d84d07b18747458eaa810af2a9b68ffc06d
DaveMcMahon <davidmcmhn@gmail.com>
2023-05-30 21:31:32 +0100

    Added a pg connection smart pointer and deserialized the database env variables from config.yaml

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
+use login_api::configuration::get_configuration;
+use login_api::startup::run;
+use sqlx::{Connection, PgConnection};
 use std::net::TcpListener;
 use std::process;
 
-use login_api::startup::run;
-
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    let database_config = get_configuration().expect("Failed to read config from config.yaml");
+    let connection = PgConnection::connect(&database_config.database.get_connection_string())
+        .await
+        .expect("Failed to connect to Postgres");
+
     let listener = TcpListener::bind("127.0.0.1:8081").unwrap_or_else(|err| {
         println!("Error binding to address {}", err);
         process::exit(1);
     });
-    run(listener)?.await
+
+    run(listener, connection)?.await
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,15 +1,18 @@
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use sqlx::PgConnection;
 use std::net::TcpListener;
 
-use crate::routes::health_check::health_check;
-use crate::routes::logins::login;
+use crate::routes::{health_check, login};
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+pub fn run(listener: TcpListener, connection: PgConnection) -> Result<Server, std::io::Error> {
+    let connection = web::Data::new(connection);
+
     let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/login", web::post().to(login))
+            .app_data(connection.clone())
     })
     .listen(listener)?
     .run();
