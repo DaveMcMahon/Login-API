e2dc379a38a06f961d86dda9f2da9bb9b113bdf3
DaveMcMahon <davidmcmhn@gmail.com>
2023-06-06 19:24:01 +0100

    Added request ID based on a uuid to make the logs corelate easier

--- src/routes/logins.rs
+++ src/routes/logins.rs
@@ -10,7 +10,18 @@ pub struct FormData {
 }
 
 pub async fn login(form: web::Form<FormData>, connection_pool: web::Data<PgPool>) -> HttpResponse {
-    log::info!("Logging new users logon details in the Database");
+    let request_id = Uuid::new_v4();
+
+    log::info!(
+        "Request ID: {} -> {} {} is logging in..",
+        request_id,
+        form.name,
+        form.email
+    );
+    log::info!(
+        "Request ID: {} -> Logging new users logon details in the Database",
+        request_id
+    );
 
     match sqlx::query!(
         r#"
@@ -26,11 +37,18 @@ pub async fn login(form: web::Form<FormData>, connection_pool: web::Data<PgPool>
     .await
     {
         Ok(_) => {
-            log::info!("Users details weree stored successfulyly in the database");
+            log::info!(
+                "Request ID: {} -> Users details were stored successfully in the database",
+                request_id
+            );
             HttpResponse::Ok().finish()
         }
         Err(e) => {
-            log::error!("Failed to execute query -> {:?}", e);
+            log::error!(
+                "Request ID: {} -> Failed to execute query -> {:?}",
+                request_id,
+                e
+            );
             HttpResponse::InternalServerError().finish()
         }
     }
